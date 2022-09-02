d657636d6e6d7f548432f81817a8ad454717c7d7
akiko616 <socialbf616@gmail.com>
2022-09-02 16:10:50 +0900

    RacingCar 리스폰문제 수정, ai차량 추가 등
    ㄱㄱ

---
+++ UnrealTeamProject/Intermediate/Source/UnrealTeamProject.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class UnrealTeamProject : ModuleRules
+{
+	public UnrealTeamProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
